ce79b1325a815bafb49589b288991bb05089827f
Nicolas BOITEUX <nicolas_boiteux@yahoo.fr>
2012-08-31 13:17:25 +0200

    First release

---
+++ PortSerialWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO.Ports;
+using Arma2Net.AddInProxy;
+
+namespace PortSerialWriter
+{
+    [AddIn("PortSerialWrite")]
+    public class PortSerialWrite : AddIn
+    {
+        public override string Invoke(string args, int maxResultSize)
+        {
+        SerialPort _serialPort;
+
+        _serialPort = new SerialPort();
+        _serialPort.PortName = "COM17";
+        _serialPort.ReadTimeout = 500;
+        _serialPort.WriteTimeout = 500;
+
+        _serialPort.Open();
+        _serialPort.WriteLine(args);
+        _serialPort.Close();
+        return args;
+        }
+    }
+}
